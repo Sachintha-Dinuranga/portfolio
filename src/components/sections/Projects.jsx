03496609c26e42b0c0ea60832add88d8da874c32
Sachintha Dinuranga <117833055+Sachintha-Dinuranga@users.noreply.github.com>
2025-03-05 13:34:59 +0530

    adjust some minor changes

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -170,7 +170,7 @@ const Projects = () => {
                 And of course, my portfolio project! 🤗
               </p>
               <div className="flex flex-wrap gap-2 mb-4">
-                {["Python", "Pygame"].map((tech, key) => (
+                {["React", "Tailwind CSS"].map((tech, key) => (
                   <span
                     key={key}
                     className="bg-blue-500/10 text-blue-500 py-1 px-3 rounded-full text-sm hover:bg-blue-500/20 hover:shadow-[0_2px_8px_rgba(59,130,2246,0.2)] transition"
